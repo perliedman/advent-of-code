fb88dc889081a840c9b106b3fd4dde385b634475
Per Liedman <per@liedman.net>
2016-12-07 07:09:00 +0100

    Remove commented code

--- day05.c
+++ day05.c
@@ -9,34 +9,6 @@
 #  include <openssl/md5.h>
 #endif
 
-/*
-char *str2md5(const char *str, int length, MD5_CTX c) {
-    int n;
-    MD5_CTX c;
-    unsigned char digest[16];
-
-    MD5_Init(&c);
-
-    while (length > 0) {
-        if (length > 512) {
-            MD5_Update(&c, str, 512);
-        } else {
-            MD5_Update(&c, str, length);
-        }
-        length -= 512;
-        str += 512;
-    }
-
-    MD5_Final(digest, &c);
-
-    for (n = 0; n < 16; ++n) {
-        snprintf(&(out[n*2]), 16*2, "%02x", (unsigned int)digest[n]);
-    }
-
-    return out;
-}
-*/
-
 void digest2hex(unsigned char *digest, char *out) {
     for (int n = 0; n < 16; ++n) {
         snprintf(&(out[n*2]), 16*2, "%02x", (unsigned int)digest[n]);
