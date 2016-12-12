14751a35fce2f7b31560e83b2ec09a71a1da264c
Per Liedman <per.liedman@dotnetmentor.se>
2016-12-12 21:20:07 +0100

    WTF

--- day11.py
+++ day11.py
@@ -4,7 +4,7 @@ import sys
 import re
 import time
 import itertools
-from collections import deque
+from collections import deque, OrderedDict
 
 f = open(sys.argv[1], 'r')
 
@@ -16,7 +16,7 @@ floorCount = 1
 equipmentCount = 0
 for line in f.readlines():
     equipment = equipment_pattern.findall(line)
-    floor = []
+    floor = OrderedDict()
     initial_floors.append(floor)
     for e in equipment:
         parts = e.split()
@@ -27,7 +27,7 @@ for line in f.readlines():
         else:
             raise Exception('Unknown equipment type "' + parts[1] + '"')
 
-        floor.append(eqId)
+        floor[eqId] = True
         equipmentCount += 1
 
     floorCount += 1
@@ -50,15 +50,22 @@ def isValid(floors, f, t):
 def createFloors(floors, equipment, fromFloor, toFloor):
     newFloors = []
     for f in floors:
-        newFloors.append(f[:])
-    newFloors[fromFloor - 1] = [eqId for eqId in floors[fromFloor - 1] if not eqId in equipment]
-    newFloors[toFloor - 1] += equipment
+        newFloors.append(OrderedDict(f))
+    for eqId in equipment:
+        del newFloors[fromFloor - 1][eqId]
+        newFloors[toFloor - 1][eqId] = True
 
     return newFloors
 
 def possibleMoves(floor):
-    one = [[eqId] for eqId in floor]
-    two = list(itertools.combinations(floor, 2))
+    equipment = floor.keys()
+    one = [[eqId] for eqId in equipment]
+    two = list(itertools.combinations(equipment, 2))
+
+    pairs = [(p1,p2) for (p1, p2) in two if p1[0:2] == p2[0:2]]
+    for p in pairs[1:]:
+        two.remove(p)
+
     return one + two
 
 def moveId(move, f, t):
@@ -70,34 +77,44 @@ def play(elevatorFloor, floors):
     queue = deque()
     queue.append((elevatorFloor, floors, []))
 
+    states = set()
+    ignored = 0
+
     while len(queue):
         (elevatorFloor, floors, moves) = queue.popleft()
         floorI = elevatorFloor - 1
 
         examinedMoves += 1
-        if (examinedMoves % 10000 == 0):
-            print examinedMoves, ' moves examined at depth ', len(moves)
+        if examinedMoves % 10000 == 0:
+            print examinedMoves, 'moves examined at depth', len(moves), ',', len(states), 'seen states,', ignored, 'dupes ignored states'
 
         candFloors = [f for f in [elevatorFloor + 1, elevatorFloor - 1] if f > 0 and f < 5]
+        possible = possibleMoves(floors[floorI])
         for nextFloor in candFloors:
-            possible = possibleMoves(floors[floorI])
-            filteredPossible = [m for m in possible if not moveId(m, elevatorFloor, nextFloor) in moves]
-#            if len(filteredPossible) == 0:
-#                print 'No possible moves at ', len(moves)
-    #        if len(possible) != len(filteredPossible):
-    #            print 'Removed ', len(possible) - len(filteredPossible)
-            for move in filteredPossible:
+            for move in possible:
                 newFloors = createFloors(floors, move, elevatorFloor, nextFloor)
                 if len(newFloors[3]) == equipmentCount:
+                    print len(states), 'seen states,', examinedMoves, 'moves examined'
                     return moves + [move]
-                elif isValid(floors, elevatorFloor, nextFloor):
-                    queue.append((nextFloor, newFloors, moves + [moveId(move, elevatorFloor, nextFloor)]))
+                else:
+                    h = str(nextFloor) + ';' + ';'.join([','.join(f) for f in newFloors])
+                    if not h in states:
+                        if isValid(floors, elevatorFloor, nextFloor):
+                            states.add(h)
+                            queue.append((nextFloor, newFloors, moves + [moveId(move, elevatorFloor, nextFloor)]))
+                    else:
+                        ignored += 1
     
+    print len(states), 'seen states,', examinedMoves, 'moves examined'
     return None
 
 best = play(1, initial_floors)
-print best
-print len(best)
+
+if best:
+    print best
+    print len(best)
+else:
+    print 'No solution found :('
 
 #printFloors(createFloors(floors, ['HyM', 'LiM'], 1, 2), 2)
 #print possibleMoves(floors[0])
