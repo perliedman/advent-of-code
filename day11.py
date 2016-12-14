ea4dd0ba301d6335c1438e723ede12b80be1a2e0
Per Liedman <per.liedman@dotnetmentor.se>
2016-12-14 12:49:04 +0100

    Finally working day 11

--- day11.py
+++ day11.py
@@ -2,9 +2,8 @@
 
 import sys
 import re
-import time
 import itertools
-from collections import deque, OrderedDict
+import heapq
 
 f = open(sys.argv[1], 'r')
 
@@ -23,6 +22,11 @@ def getId(name):
         index = len(eqMap) + 1
         eqMap[name] = index
         return index
+
+def correct(floor):
+    gens = [c for c in floor if c < 0]
+    chipsWoGen = [c for c in floor if c > 0 and not -c in floor]
+    return len(gens) == 0 or len(chipsWoGen) == 0
 
 for line in f.readlines():
     equipment = equipment_pattern.findall(line)
@@ -43,10 +47,41 @@ for line in f.readlines():
 
         floor.append(eqId)
 
-    initial_floors[floorCount - 1] = sorted(initial_floors[floorCount - 1])
+    initial_floors[floorCount - 1] = tuple(sorted(initial_floors[floorCount - 1]))
     floorCount += 1
 
 print eqMap
 print initial_floors
 
+initial = (0, tuple(initial_floors))
 
+q = []
+heapq.heappush(q, (0, initial))
+costs = {
+    initial: 0
+}
+
+while q:
+    _, current = heapq.heappop(q)
+
+    (elevator, floors) = current
+
+    if len(floors[3]) == equipmentCount:
+        break
+
+    possibleMoves = [(x,) for x in floors[elevator]] + list(itertools.combinations(floors[elevator], 2))
+    newCost = costs[current] + 1
+
+    for nextFloor in [f for f in [elevator + 1, elevator - 1] if f >= 0 and f <= 3]:
+        for m in possibleMoves:
+            nextFloors = list(floors)
+            nextFloors[nextFloor] = tuple(sorted(floors[nextFloor] + m))
+            nextFloors[elevator] = tuple([eqId for eqId in floors[elevator] if eqId not in m])
+
+            if correct(nextFloors[nextFloor]) and correct(nextFloors[elevator]):
+                nextState = (nextFloor, tuple(nextFloors))
+                if nextState not in costs or newCost < costs[nextState]:
+                    heapq.heappush(q, (newCost - len(nextFloors[3]) * 3, nextState))
+                    costs[nextState] = newCost
+
+print costs[current]
