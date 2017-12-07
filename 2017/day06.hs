554f1b6828e134183e98766293f30290ffff10bb
Per Liedman <per.liedman@dotnetmentor.se>
2017-12-07 20:09:26 +0100

    Day 6

--- 2017/day06.hs
+++ 2017/day06.hs
@@ -5,22 +5,29 @@ maxIndex xs = let
   (maxVal, i, _) = foldl (\(maxVal, maxIndex, i) x -> if x > maxVal then (x, i, i + 1) else (maxVal, maxIndex, i + 1)) (-1, -1, 0) xs
   in (i, maxVal)
 
-cycleMaxFirst xs = let
-  (mIndex, mVal) = maxIndex xs
-  h = drop (mIndex + 1) xs
-  t = take mIndex xs
-  in mVal : h ++ t
+spread amount step xs = let
+  f (rest, ys) x = let
+    s = if rest > step then step else rest
+    in (rest - s, ys ++ [x + s])
+  in foldl f (amount, []) xs
 
-nextState (x:xs) = let
+nextState xs = let
   l = length xs
-  spread = x `div` l
-  remainder = x `mod` l
-  in remainder : (map (\x -> x + spread) xs)
+  (mIndex, mVal) = maxIndex xs
+  value = max 1 $ if mVal `mod` l == 0 then mVal `div` l else mVal `div` (l - 1)
+  (tr, t) = spread mVal value $ drop (mIndex + 1) xs
+  (remainder, h) = spread tr value $ take mIndex xs
+  in h ++ [remainder] ++ t
+  -- in (h ++ [remainder] ++ t, mIndex, mVal, value, remainder, h, t)
 
 countCycles xs seen count = let
-  cycled = cycleMaxFirst xs
-  hasBeenSeen = S.member cycled seen
-  in if hasBeenSeen then count else countCycles (nextState cycled) (S.insert cycled seen) (count + 1)
+  hasBeenSeen = S.member xs seen
+  in if hasBeenSeen then count else countCycles (nextState xs) (S.insert xs seen) (count + 1)
 
 genStates _ 0 = []
-genStates xs n = let c = cycleMaxFirst xs in c : genStates (nextState c) (n - 1)
+genStates xs n = xs : genStates (nextState xs) (n - 1)
+
+a xs = countCycles xs S.empty 0
+b xs = let
+  state = last $ genStates xs (a xs + 1)
+  in a state
