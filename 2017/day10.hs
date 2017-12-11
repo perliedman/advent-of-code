358a610725ebb08c28fa9b0720ba6c302d7be4b4
Per Liedman <per.liedman@dotnetmentor.se>
2017-12-11 20:31:19 +0100

    WIP

--- 2017/day10.hs
+++ 2017/day10.hs
@@ -1,4 +1,8 @@
 import Debug.Trace
+import Data.Char
+import Data.Bits
+import Data.List.Split
+import Numeric
 
 -- revcycle len l skip xs | trace ("revcycle " ++ 
 --   " h=" ++ show (take (len+skip) xs) ++
@@ -20,4 +24,21 @@ pinchtwist len (xs, p, skip) [] = (take len $ drop (len - (p `mod` len)) xs, p, 
 pinchtwist len (xs, p, skip) (l:ls) = let
   in pinchtwist len (revcycle len l skip xs, p + l + skip, skip + 1) ls
 
+stdsuffix :: [Int]
 stdsuffix = [17, 31, 73, 47, 23]
+
+sparseHash l = let
+  (h, _, _) = foldl (\(xs, p, skip) _ -> pinchtwist 256 (cycle xs, p, skip) l) ([0..255], 0, 0) [1..64]
+  in h
+
+strToLengths s = map ord s ++ stdsuffix
+
+denseHash :: [Int] -> [Int]
+denseHash xs = let
+  xorGroup gs = foldl1 (\a x -> a `xor` x) gs
+  in map xorGroup $ chunksOf 16 xs
+
+toHexStr xs = concat $ map (\x -> if x < 16 then "0" ++ showHex x "" else showHex x "") xs
+
+hash :: String -> String
+hash = toHexStr . denseHash . sparseHash . strToLengths
