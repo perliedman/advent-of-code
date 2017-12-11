1659544785a7b4a24889b5f5e2db2e72350566e8
Per Liedman <per.liedman@dotnetmentor.se>
2017-12-11 20:43:52 +0100

    Day 10 complete

--- 2017/day10.hs
+++ 2017/day10.hs
@@ -4,31 +4,20 @@ import Data.Bits
 import Data.List.Split
 import Numeric
 
--- revcycle len l skip xs | trace ("revcycle " ++ 
---   " h=" ++ show (take (len+skip) xs) ++
---   " t=" ++ show (reverse $ take l xs) ++
---   " =>" ++ show ((take len xs) ++ (reverse $ take l xs))) False = undefined
 revcycle len l skip xs = let
   h = take len xs
   t = reverse $ take l xs
   in drop skip $ cycle $ drop l (h ++ t)
 
 pinchtwist :: Int -> ([Int], Int, Int) -> [Int] -> ([Int], Int, Int)
--- pinchtwist len (xs, p, skip) (l:ls) | trace ("pinchtwist " ++ 
---   show len ++ " " ++ show p ++ " " ++ show skip ++
---   " h=" ++ show (take (len+skip) xs) ++
---   " t=" ++ show (reverse $ take l xs) ++
---   " =>" ++ show ((take (len+skip) xs) ++ (reverse $ take l xs))) False = undefined
--- pinchtwist len (xs, p, skip) [] | trace ("pinchtwist " ++ show len ++ " " ++ show p ++ " " ++ show skip) False = undefined
 pinchtwist len (xs, p, skip) [] = (take len $ drop (len - (p `mod` len)) xs, p, skip)
 pinchtwist len (xs, p, skip) (l:ls) = let
   in pinchtwist len (revcycle len l skip xs, p + l + skip, skip + 1) ls
 
-stdsuffix :: [Int]
 stdsuffix = [17, 31, 73, 47, 23]
 
 sparseHash l = let
-  (h, _, _) = foldl (\(xs, p, skip) _ -> pinchtwist 256 (cycle xs, p, skip) l) ([0..255], 0, 0) [1..64]
+  (h, _, _) = foldl (\(xs, p, skip) _ -> pinchtwist 256 (drop p $ cycle xs, p, skip) l) ([0..255], 0, 0) [1..64]
   in h
 
 strToLengths s = map ord s ++ stdsuffix
