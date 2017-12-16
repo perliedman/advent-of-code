a94406f1f039e0c527a6ce3f9444f5b1e6085d3e
Per Liedman <per.liedman@dotnetmentor.se>
2017-12-16 16:42:31 +0100

    Day 14 + break out KnotHash

---
+++ 2017/KnotHash.hs
@@ -0,0 +1,34 @@
+module KnotHash (hash) where
+
+import Debug.Trace
+import Data.Char
+import Data.Bits
+import Data.List.Split
+import Numeric
+
+revcycle len l skip xs = let
+  h = take len xs
+  t = reverse $ take l xs
+  in drop skip $ cycle $ drop l (h ++ t)
+
+pinchtwist :: Int -> ([Int], Int, Int) -> [Int] -> ([Int], Int, Int)
+pinchtwist len (xs, p, skip) [] = (take len $ drop (len - (p `mod` len)) xs, p, skip)
+pinchtwist len (xs, p, skip) (l:ls) = pinchtwist len (revcycle len l skip xs, p + l + skip, skip + 1) ls
+
+stdsuffix = [17, 31, 73, 47, 23]
+
+sparseHash l = let
+  (h, _, _) = foldl (\(xs, p, skip) _ -> pinchtwist 256 (drop p $ cycle xs, p, skip) l) ([0..255], 0, 0) [1..64]
+  in h
+
+strToLengths s = map ord s ++ stdsuffix
+
+denseHash :: [Int] -> [Int]
+denseHash xs = let
+  xorGroup gs = foldl1 xor gs
+  in map xorGroup $ chunksOf 16 xs
+
+toHexStr xs = concat $ map (\x -> if x < 16 then "0" ++ showHex x "" else showHex x "") xs
+
+hash :: String -> String
+hash = toHexStr . denseHash . sparseHash . strToLengths
---
+++ 2017/day14.hs
@@ -0,0 +1,55 @@
+import KnotHash
+import Data.List.Split
+import Data.Char
+import Numeric
+import Data.Bits
+import Data.Array
+
+countHashBits = sum . map ((\x -> popCount (x :: Integer)) . fst . head . readHex)
+knotHashes i = map (\x -> hash $ i ++ "-" ++ (show x)) [0..127]
+
+a = countHashBits . knotHashes
+
+nibbleToBits x = let
+  toInt False = 0 :: Int
+  toInt True = 1 :: Int
+  f x n = if n >= 0 then (toInt $ testBitDefault x n):(f x (n-1)) else []
+  in f x 3
+
+digitsToBits = map ((\x -> nibbleToBits (x :: Int)) . fromIntegral . fst . head . readHex)
+hexStrToDigits = filter (\x -> x /= "") . splitOn ""
+hexStrToBits = concat . digitsToBits . hexStrToDigits
+hashesToBits = map hexStrToBits
+
+toComplexArray :: [[a]] -> Array(Int, Int) a
+toComplexArray grid = array ((0,0),((length $ grid !! 0) - 1,(length grid) - 1))  entries  
+  where entries = concatMap (\z -> map (\y -> ((fst y, fst z), snd y))  (snd z)) $ zip [0..] $ map (\x -> zip [0..] x) grid
+
+inBounds :: Array (Int, Int) Int -> (Int, Int) -> Bool
+inBounds grid (x, y) = x >= lowx && x <= highx && y >= lowy && y <= highy
+  where ((lowx, lowy), (highx, highy)) =  bounds grid
+ 
+replace :: Array (Int, Int) Int -> (Int, Int) -> Int -> Array (Int, Int) Int
+replace grid point replacement = if inBounds grid point then grid // [(point, replacement)] else grid
+floodFill :: Array (Int, Int) Int ->  (Int, Int) -> Int -> Int -> Array (Int, Int) Int
+floodFill grid point@(x, y) target replacement =
+  if((not $ inBounds grid point) ||  grid ! (x,y) /= target) then grid 
+  else 
+    gridNorth
+    where grid' = replace grid point replacement
+          gridEast = floodFill grid' (x+1, y) target replacement
+          gridWest = floodFill gridEast (x-1, y) target replacement
+          gridSouth = floodFill gridWest (x, y+1) target replacement
+          gridNorth = floodFill gridSouth (x, y-1) target replacement
+
+fillGroups grid = let
+  ((_, _), (w, h)) = bounds grid
+  fillGroup grid (x, y) gn = let
+    v = grid ! (x, y)
+    nextC = if x < w then (x+1, y) else (0, y+1)
+    filled = if v == 1 then floodFill grid (x, y) 1 gn else grid
+    in if y<=h then fillGroup filled nextC (if v == 1 then gn+1 else gn) else (grid, gn)
+  (filledGrid, ngroups) = fillGroup grid (0, 0) 2
+  in (filledGrid, ngroups - 2)
+
+b = snd . fillGroups . toComplexArray . hashesToBits . knotHashes
